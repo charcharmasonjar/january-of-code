1fca868e45efc97b6401a3effacfcf25d5897497
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-03 18:48:12 -0800

    Day 6 part 2 🛰

--- day6.js
+++ day6.js
@@ -4,6 +4,7 @@ let data = fs
   .toString()
   .split("\n");
 
+// part 1
 // create object with satellites as keys
 const makeObject = function(arr) {
   const obj = {};
@@ -18,11 +19,12 @@ const countPath = function(sat, obj) {
   let count = 0;
   let currentSat = sat;
   while (obj[currentSat]) {
-    currentSat = obj[currentSat]
-    count ++;
+    currentSat = obj[currentSat];
+    count++;
   }
   return count;
-}
+};
+
 
 // call count function for each key in the object
 const countPaths = function(obj) {
@@ -32,7 +34,38 @@ const countPaths = function(obj) {
     count += countPath(item, obj);
   }
   return count;
-}
+};
+
+// part 2
+// creates array of steps for a given satellite back to the center
+const getPath = function(sat, obj) {
+  const path = [];
+  let currentSat = sat;
+  while (obj[currentSat]) {
+    path.push(currentSat);
+    currentSat = obj[currentSat];
+  }
+  return path;
+};
+
+// compares two arrays to find closest common ancestor
+// returns sum - 2 to get total steps
+const getAncestor = function(sat1, sat2) {
+  for (let i = 0; i < sat1.length; i++) {
+    for (let j = 0; j < sat2.length; j++) {
+      if (sat1[i] === sat2[j]) {
+        return i + j - 2;
+      }
+    }
+  }
+};
 
 const obj = makeObject(data);
+
+// part 1 answer
 console.log(countPaths(obj));
+
+// part 2 answer
+const youPath = getPath("YOU", obj);
+const sanPath = getPath("SAN", obj);
+console.log(getAncestor(youPath, sanPath));
